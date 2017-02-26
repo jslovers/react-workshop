e36893fee8ebbea267ff42675b4727e041eef7ef
Jaswant Singh <jaswantsingh@inlaptop51s-MacBook-Pro.local>
2017-02-26 13:39:54 +0530

    Stateless components with props.

--- src/components/application.jsx
+++ src/components/application.jsx
@@ -3,11 +3,16 @@ import Content from './content-page';
 
 export default class Application extends Component {
     render()
-    {
+    {   
+        const lorem = "Lorem ipsum";
+        const content = `${lorem} dolor sit amet, consectetur adipiscing elit. Duis congue facilisis
+            orci. Integer nulla neque, egestas at condimentum nec, interdum et dolor. Cras
+            vulputate, elit at vulputate eleifend, dolor turpis consectetur justo,`
+
         return (
             <div>
                 <h3>Hi Friends, I am component with state less component as child.</h3>
-                <div><Content/></div>
+                <div><Content fontSize={18} content={content}/></div>
             </div>
         )
     }
